f191e0f3ed91e19df3c169c351aaf680e12adceb
Nicolás Salerno <amplificodora04@gmail.com>
2026-08-20 14:44:56 -0300

    first commit

---
+++ freya/src/test/java/com/example/freya/FreyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
