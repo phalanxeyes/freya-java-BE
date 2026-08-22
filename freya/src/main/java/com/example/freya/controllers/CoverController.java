eb7a333a8d6dc6d1d9068d75cadd432c044d1c81
Nicolás Salerno <amplificodora04@gmail.com>
2026-08-22 20:46:27 -0300

    added update, changed fecha_hora to fechaHora

--- freya/src/main/java/com/example/freya/controllers/CoverController.java
+++ freya/src/main/java/com/example/freya/controllers/CoverController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,11 @@ public class CoverController {
     @PostMapping
     public Cover create(@RequestBody Cover cover) {
         return coverService.create(cover);
+    }
+    
+    @PutMapping("/{coverId}")
+    public Cover update(@PathVariable("coverId") Integer coverId, @RequestBody Cover coverDetails){
+    	return coverService.update(coverId, coverDetails);
     }
     
     @DeleteMapping("/{coverId}")
--- freya/src/main/java/com/example/freya/entities/Cover.java
+++ freya/src/main/java/com/example/freya/entities/Cover.java
@@ -4,20 +4,24 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name= "covers")
 public class Cover {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private Integer duracion_cover;
+	private Integer duracion;
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-	private LocalDateTime fecha_hora_cover;
+	@Column(name = "fecha_hora")
+	private LocalDateTime fechaHora;
 	private Integer reproducciones;
 	
 	public Integer getId() {
@@ -26,17 +30,17 @@ public class Cover {
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public Integer getDuracion_cover() {
-		return duracion_cover;
+	public Integer getDuracion() {
+		return duracion;
 	}
-	public void setDuracion_cover(Integer duracion_cover) {
-		this.duracion_cover = duracion_cover;
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
 	}
-	public LocalDateTime getFecha_hora_cover() {
-		return fecha_hora_cover;
+	public LocalDateTime getFechaHora() {
+		return fechaHora;
 	}
-	public void setFecha_hora_cover(LocalDateTime fecha_hora_cover) {
-		this.fecha_hora_cover = fecha_hora_cover;
+	public void setFechaHora(LocalDateTime fechaHora) {
+		this.fechaHora = fechaHora;
 	}
 	public Integer getReproducciones() {
 		return reproducciones;
@@ -45,3 +49,5 @@ public class Cover {
 		this.reproducciones = reproducciones;
 	}
 }
+
+
